2e223e503be5ac9948ac98c8bad6e846d79445ef
sina <sinadsina78@gmail.com>
2026-05-23 23:01:49 +0330

    exercise one

---
+++ 1/main_beyond_2.py
@@ -0,0 +1,32 @@
+import random
+
+def guessing_game():
+    answer = random.randint(0,100)
+    base = random.randint(2, 16)
+
+
+    print(f'answer is:{answer}---{base}')
+    c = 0
+    while c < 3:
+        user_answer = input(f"Guess the number (in base {base}): ")
+
+        try:
+            guess = int(user_answer, base)
+        except ValueError:
+            print(f"Invalid input! Please enter a valid number for base {base}.")
+            continue
+
+        if guess == answer:
+            print('you guess exactly right') 
+            break
+        elif guess > answer:
+            print(f'{guess} is too high') 
+            c = c+1
+        else :
+            print(f'{guess} is too low') 
+            c = c+1
+    if c ==3 :
+        print('ran out of guess!!')
+guessing_game()
+
+
---
+++ 1/main_beyond_3.py
@@ -0,0 +1,28 @@
+import random
+
+def word_guessing_game():
+    # mac/linux users only
+    with open('/usr/share/dict/words', 'r') as f:
+
+        all_words = [word.strip().lower() for word in f.readlines()]
+        filtered_words = [w for w in all_words if 2 <= len(w) <= 5]
+
+    answer = random.choice(filtered_words)
+    print(answer)
+    while True:
+        guess = input(f"Guess the word(only 2 to 5 letters words): ")
+
+        
+        if guess == answer:
+            print('you guess exactly right') 
+            break
+        elif guess > answer:
+            print(f'{guess} is earlier in dic') 
+          
+        else :
+            print(f'{guess} is later in dic') 
+            
+
+word_guessing_game()
+
+
