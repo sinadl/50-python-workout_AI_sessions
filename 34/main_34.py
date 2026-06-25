61cc7ac47b188dd4bec37b0ac4d59343e88cc887
sina <sinadsina78@gmail.com>
2026-06-26 03:04:38 +0330

    feat(34:supervocalic words): find vowels words from a file

---
+++ 34/main_34.py
@@ -0,0 +1,11 @@
+def get_sv(filename):
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+
+    return {
+        word.strip()
+        for word in open(filename)
+        if vowels <= set(word.lower())
+    }
+    
+    
+print(get_sv('34/words.txt'))
