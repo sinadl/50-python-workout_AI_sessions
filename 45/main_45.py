cbcce99976f02f89f2cfe1bffbe2defc785f8d41
sina <sinadsina78@gmail.com>
2026-07-04 04:00:42 +0330

    feat(45): concepts of OOP

--- 45/main_45.py
+++ 45/main_45.py
@@ -33,8 +33,38 @@ class cage():
         output = f'cage: {self.id_number}\n'
         output += '\n'.join('\t' + str(animal) for animal in self.animals)
         return output
+
+class Zoo():
+    def __init__(self):
+        self.cages = []
+        
+    def add_cages(self, *cages):
+        for one_cage in cages:
+            self.cages.append(one_cage)
+
+
+    def __repr__(self):
+        return '\n'.join(str(one_cage)
+        for one_cage in self.cages)
         
         
+    def animals_by_color(self, color):
+        return [one_animal
+        for one_cage in self.cages
+        for one_animal in one_cage.animals
+        if one_animal.color == color]
+        
+        
+    def animals_by_legs(self, number_of_legs):
+        return [one_animal
+        for one_cage in self.cages
+        for one_animal in one_cage.animals  
+        if one_animal.number_of_legs ==[number_of_legs]]
+    def number_of_legs(self):
+        return sum(one_animal.number_of_legs
+            for one_cage in self.cages
+                for one_animal in one_cage.animals)
+
 wolf = Wolf('black')
 sheep = Sheep('white')
 snake = Snake('brown')
@@ -43,7 +73,12 @@ print(wolf)
 print(sheep)
 print(snake)
 print(parrot)
-
 c1 = cage(1)
-c1.add_animal(wolf,sheep,snake)
-print(c1)
+c1.add_animal(wolf, sheep)
+c2 = cage(2)
+c2.add_animal(snake, parrot)
+z = Zoo()
+z.add_cages(c1, c2)
+print(z)
+print(z.animals_by_color('white'))
+print(z.animals_by_legs(4))
